3a676318d498a3251eac755a2bc2595ffafde461
chinmay jain <chinmay874@gmail.com>
2023-04-19 23:30:45 +0530

    assignment of separate component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from './button/button';
+import { Benifit } from './benifits/benifit';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Benifits of using reacts...</h1>
-        <p>
-          1. Component-based architecture<br />
-          2. Virtual Dom for efficient updates <br />
-          3. Rich ecosystem and community <br />
-          4. Cross-platform development <br />
-          5. Strong community support <br />
-        </p>
-        <button>Get Started...</button>
+        <Benifit />
+        <Button />
       </header>
     </div>
   );
---
+++ src/benifits/benifit.js
@@ -0,0 +1,16 @@
+import './benifit.css'
+
+export function Benifit(){
+    return (
+        <div>
+        <h1>Benifits of using reacts...</h1>
+        <p>
+          1. Component-based architecture<br />
+          2. Virtual Dom for efficient updates <br />
+          3. Rich ecosystem and community <br />
+          4. Cross-platform development <br />
+          5. Strong community support <br />
+        </p>
+        </div>
+    )
+}
